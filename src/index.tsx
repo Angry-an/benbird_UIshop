c1d08c9330ad4917d1166f9b186b81c69f822073
qian_xiaoan <519637185@qq.com>
2021-04-30 17:27:56 +0800

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import Router from './router'
+ReactDOM.render(
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
